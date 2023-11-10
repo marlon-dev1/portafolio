7e45f04b9165e08f94935c1419d9573028b24587
Marlon Varon <mvaron@ccxc.us>
2023-11-09 20:55:48 -0500

    personalized portfolio

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const NavLink = ({ href, title }) => {
-  return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-    >
-      {title}
-    </Link>
-  );
+    return (
+        <Link
+            href={href}
+            className="block py-2 pl-3 pr-4 text-transparent rounded hover:border-b hover:border-purple-500 sm:text-xl md:p-0 bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600"
+        >
+            {title}
+        </Link>
+    );
 };
 
 export default NavLink;
